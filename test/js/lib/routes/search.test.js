83a923afbd1f01c0110e1afd31fa734bcfd9d88c
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2018-05-06 16:14:27 +0100

    Add search route

---
+++ test/js/lib/routes/search.test.js
@@ -0,0 +1,41 @@
+const expect = require('chai').use(require('chai-as-promised')).expect;
+const sinon = require('sinon');
+const search = require('../../../../lib/api/routes/search');
+
+let req;
+let res;
+let next;
+
+describe('Search route', () => {
+    beforeEach(() => {
+        req = { query: { term: 'search term' } };
+        res = {
+            locals: {
+                spotifyApi: {
+                    search: sinon.stub().resolves('results go here')
+                }
+            },
+            json: sinon.stub()
+        };
+        next = sinon.stub();
+    });
+
+    it('searches spotify\'s API for the requested search term', async () => {
+        await search(req, res, next);
+
+        sinon.assert.calledWith(res.locals.spotifyApi.search, 'search term', ['album', 'artist', 'track'], {'market': 'GB'});
+    });
+    
+    it('outputs the search results as json', async () => {
+        await search(req, res, next);
+        
+        sinon.assert.calledWith(res.json, 'results go here');
+    });
+
+    it('calls next with an error if there is a problem', async () => {
+        res.locals.spotifyApi.search = sinon.stub().throws('this is an error')
+        await search(req, res, next);
+
+        sinon.assert.calledWith(next, sinon.match.instanceOf(Error));
+    });
+});
